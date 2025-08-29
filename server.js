3e37b14c2b76ab75115154e4da86a9f2edd0c064
Eduardo Lopes <eduardolcb18@gmail.com>
2025-08-29 12:42:50 -0300

    comentários sobre o uso do fastify

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const crypto = require('crypto');
 const fastify = require('fastify');
 
 //criação do servidor
+//uma variável recebe a classe fastify para ter acesso a todos os seus métodos
 const server = fastify({
-    logger: true
+    logger: true //o fastify pode receber parametros para ativar funcionalidades 
 
 });
 
@@ -15,6 +16,10 @@ const courses = [
     {id: '3', title : 'Curso de React Native'},
 ]
 
+
+//criação de rotas, a variavel principal utiliza o método de requisição HTTP
+//dentro de sua função pode receber dois parametros
+//o primeiro parametro se da a "rota", o segundo uma arrow function call back a se execultada
 server.get('/courser', () =>{
     return courses
 })
@@ -31,6 +36,11 @@ server.post('/courser', (request, reply)=>{
     return reply.status(201).send({ courseID })
 });
 
+
+//configuração do servidor. 
+//a variável principal que contém todos os métodos do fastify
+//ela usa o métodolisten que recebe um parametro da porta
+//o then execulta a arrow function call back quando a promisse for concluída 
 
 server.listen({port:3333}).then(
     () =>{
